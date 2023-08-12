e95fd6d8aa5e5b6739cd8d0976ac7befd42b5084
JD Bothma <jbothma@gmail.com>
2023-08-12 19:30:35 +0200

    start adding categorisation

--- ocds_data_summary/core/models.py
+++ ocds_data_summary/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
-# Create your models here.
+
+class Summary(TimeStampedModel):
+    data = models.JSONField()
+
+
+class Category(TimeStampedModel):
+    label = models.CharField(max_length=255)
+
+    my_order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ["my_order"]
+
+
+class Entity(TimeStampedModel):
+    label = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
--- ocds_data_summary/settings.py
+++ ocds_data_summary/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django_extensions",
     "django_q",
+    "adminsortable2",
 ]
 
 MIDDLEWARE = [
