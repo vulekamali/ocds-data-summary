a3d00dcffe6d4c9cb8e440ba537ba680fe6e7e1e
JD Bothma <jbothma@gmail.com>
2023-09-07 09:29:03 +0200

    Fix repeated fetching

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,30 +12,32 @@ function App() {
   const [categoryResults, setCategoryResults] = React.useState(null);
 
   useEffect(() => {
-    const url = "https://ocds-summary-backend.vulekamali.gov.za/api/summary/latest";
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const categoryLabels = data.groups;
-        const groupedObj = data.months.reduce((accumulator, value) => {
-          if (accumulator[value.category] === undefined)
-            accumulator[value.category] = [];
-          accumulator[value.category].push(value);
-          return accumulator;
-        }, {});
+    if (categoryResults == null) {
+      const url = "https://ocds-summary-backend.vulekamali.gov.za/api/summary/latest";
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          const categoryLabels = data.groups;
+          const groupedObj = data.months.reduce((accumulator, value) => {
+            if (accumulator[value.category] === undefined)
+              accumulator[value.category] = [];
+            accumulator[value.category].push(value);
+            return accumulator;
+          }, {});
 
-        const groupedArr = categoryLabels.map(label => {
-          return {
-            "label": label,
-            "items": groupedObj[label],
-          }
+          const groupedArr = categoryLabels.map(label => {
+            return {
+              "label": label,
+              "items": groupedObj[label],
+            }
+          })
+          setCategoryResults(groupedArr);
         })
-        setCategoryResults(groupedArr);
-      })
-      .catch((error) => {
-        console.log(error)
-        alert(error)
-      });
+        .catch((error) => {
+          console.log(error)
+          alert(error)
+        });
+    }
   });
 
   return (
@@ -50,7 +52,7 @@ function App() {
         {categoryResults == null ? <Loading /> : (
           <>
             {categoryResults.map((category, i) =>
-              <Section key={category.label} heading={category.label} data={category.items} marginTop={i > 0 && 200}/>
+              <Section key={category.label} heading={category.label} data={category.items} marginTop={i > 0 && 200} />
             )}
           </>
         )}
